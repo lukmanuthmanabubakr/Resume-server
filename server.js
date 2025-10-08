6d100fea5ea58de7ae18bf423059164743072294
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-08 12:57:23 +0100

    update

--- server.js
+++ server.js
@@ -13,3 +13,10 @@ app.use(
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
+
+//Middleware
+app.use(express.json());
+
+//Start Server
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
