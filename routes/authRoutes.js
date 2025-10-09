26ec62390c73980f1d228dda6bde767ee9eac046
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-09 16:47:10 +0100

    update

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,3 +5,12 @@ const {
   getUserProfile,
 } = require("../controllers/authController");
 const { protect } = require("../middlesware/authMiddleware");
+
+const router = express.Router();
+
+//Auth Routes
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.get("/profile", protect, getUserProfile);
+
+module.exports = router;
