4e5006fd6d7b8876a2443661899e02657af06339
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-20 03:31:23 +0100

    update

--- controllers/authController.js
+++ controllers/authController.js
@@ -47,11 +47,46 @@ const registerUser = async (req, res) => {
 // @desc Login user
 // @route POST /api/auth/login
 // @access Public
-const loginUser = async (req, res) => {};
+const loginUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(500).json({ message: "Invalid email or password" });
+    }
+
+    //Compare password
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      return res.status(500).json({ message: "Invalid email or password" });
+    }
+
+    //Return user data with JWT
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      profileImageUrl: user.profileImageUrl,
+      token: generateToken(user._id),
+    });
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 // @desc Get User Profile
 // @route GET /api/auth/profile
 // @access Public(Required JWT)
-const getUserProfile = async (req, res) => {};
+const getUserProfile = async (req, res) => {
+  try {
+    const user = await User.findById(req.user.id).select("-password");
+    if (!user) {
+      return res.status(401).json({ message: "User not found" });
+    }
+    res.json(user);
+  } catch (error) {
+    res.status(500).json({ message: "Server error", error: error.message });
+  }
+};
 
 module.exports = { registerUser, loginUser, getUserProfile };
