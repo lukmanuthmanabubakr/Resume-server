6f0e25ecb5bc1f41018055b503f64fb8ec6014cb
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-10-20 22:41:32 +0100

    update

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -15,5 +15,27 @@ const createResume = async (req, res) => {
 };
 
 // @desc Get all resumes for logged-in-user
-// @route POST /api/resumes
+// @route GET /api/resumes
 // @access Private
+const getUserResumes = async (req, res) => {};
+
+// @desc Get single resume by ID
+// @route GET /api/resumes/:id
+// @access Private
+const getResumeById = async (req, res) => {};
+// @desc Update a resume
+// @route PUT /api/resumes/:id
+// @access Private
+const updateResume = async (req, res) => {};
+// @desc Delete a resume
+// @route DELETE /api/resumes/:id
+// @access Private
+const deleteResume = async (req, res) => {};
+
+module.exports = {
+  createResume,
+  getUserResumes,
+  getResumeById,
+  updateResume,
+  deleteResume,
+};
