3f17137d85a28884ab080a64e4ac4f1cb7b55a32
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-18 23:41:31 +0100

    update

--- routes/resumeRoutes.js
+++ routes/resumeRoutes.js
@@ -1,7 +1,11 @@
 import express from "express";
-import protect from "../middlewares/authMiddleware";
-import { createResume } from "../controllers/resumeController";
+import protect from "../middlewares/authMiddleware.js";
+import { createResume, updateResume } from "../controllers/resumeController.js";
+import upload from "../configs/multer.js";
 
-const resumeRouter = express.Router()
+const resumeRouter = express.Router();
 
-resumeRouter.post('/create', protect, createResume)
+resumeRouter.post("/create", protect, createResume);
+resumeRouter.put("/update", upload.single("image"), protect, updateResume);
+resumeRouter.delete("/delete/:resumeId", protect, createResume);
+
