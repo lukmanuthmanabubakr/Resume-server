1192141d07e2ba58b9e7fa28b6000f36ea8a9999
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-18 13:34:50 +0100

    update

--- controllers/resumeController.js
+++ controllers/resumeController.js
@@ -56,7 +56,6 @@ export const getResumeById = async (req, res) => {
     resume.updatedAt = undefined;
 
     return res.status(200).json({ resume });
-
   } catch (error) {
     return res.status(400).json({ message: error.message });
   }
@@ -64,3 +63,22 @@ export const getResumeById = async (req, res) => {
 
 // get resume by id public
 // GET: /api/resumes/public
+
+export const getPublicResumeById = async (req, res) => {
+  try {
+    const { resumeId } = req.params;
+
+    const resume = await Resume.findOne({ public: true, _id: resumeId });
+
+    if (!resume) {
+      return res.status(404).json({ message: "Resume not found" });
+    }
+
+    return res.status(200).json({ resume });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
+
+// controller for updating a resume
+// PUT: /api/resumes/update
