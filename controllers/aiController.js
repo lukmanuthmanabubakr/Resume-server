15978eec667cd8af9b3f849a063b33b2d9c05ecf
lukmanuthmanabubakr <lukmanuthman135@gmail.com>
2025-11-19 13:47:22 +0100

    update

--- controllers/aiController.js
+++ controllers/aiController.js
@@ -1,4 +1,35 @@
-
-
 //Controller for enhancing a resume's professional resume
 //POST: /api/ai/enhance-pro-sum
+
+import ai from "../configs/ai.js";
+
+export const enhanceProfessionalSummary = async (req, res) => {
+  try {
+    const { userContent } = req.body;
+    if (!userContent) {
+      return res.status(400).json({ message: "Missing required fields" });
+    }
+
+    const response = await ai.chat.completions.create({
+      model: process.env.OPENAI_MODEL,
+      reasoning_effort: "low",
+      messages: [
+        {
+          role: "system",
+          content:
+            "You are an expert in resume writing. Your task is to enhance the professional summary of a resume. The summary should be 1–2 sentences, highlighting key skills, experience, and career objectives. Make it compelling and ATS-friendly. Only return the improved text no options or additional explanations.",
+        },
+
+        {
+          role: "user",
+          content: userContent,
+        },
+      ],
+    });
+
+    const enhancedContent = response.choices[0].message.content;
+    return res.status(200).json({ enhancedContent });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+};
